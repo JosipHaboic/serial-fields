458b88fcf77f97325817fc155a624f28005fed39
jhaboic <josiphaboic@gmail.com>
2021-03-21 14:11:11 +0100

    add serial-ports child route to dashboard

--- src/views/dashboard/SerialPorts.vue
+++ src/views/dashboard/SerialPorts.vue
@@ -1,5 +1,41 @@
 <template>
 	<div class="container">
-		<h1 style="color: white;">Hello</h1>
+		<ul>
+			<li v-for="port in ports" :key="port.path" class="port">
+				<router-link class="link" :to="route(port.path)">
+					<div class="port-path">{{ port.path }}</div>
+					<div class="port-status">{{ portStatus(port.path) }}</div>
+				</router-link>
+			</li>
+		</ul>
+		<router-view></router-view>
 	</div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	data: () => ({
+		ports: [
+			{ path: 'COM5',},
+			{ path: 'COM3',},
+			{ path: 'COM1',},
+		]
+	}),
+	methods: {
+		portStatus(portPath?: string) {
+			return "OPEN";
+		},
+		route: (path: string) => {
+			return `/serial-ports/${path}`;
+		}
+	},
+});
+</script>
+
+<style scoped>
+.port {
+	color: var(--color-0-1);
+}
+</style>
