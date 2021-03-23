1a57609b1c33642fc4bbe8d978638f33757b10cb
jhaboic <josiphaboic@gmail.com>
2021-03-23 23:25:46 +0100

    add cable icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,8 @@
 import USBInterfaceIcon from './USBInterfaceIcon.vue';
+import CableIcon from './CableIcon.vue';
 
 
 export const icons = {
 	'usb-interface': USBInterfaceIcon,
+	'cable': CableIcon,
 };
