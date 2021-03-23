d0b0432f313044c96ebe7b8eb02a29dcd0f0d70c
jhaboic <josiphaboic@gmail.com>
2021-03-23 12:32:58 +0100

    rework component so we can get port status

--- src/views/dashboard/SerialPorts.vue
+++ src/views/dashboard/SerialPorts.vue
@@ -1,33 +1,60 @@
 <template>
 	<div class="container">
-		<div class="port-list">
-			<div v-for="port in ports" :key="port.path" class="port-list-item">
-				<router-link class="link" :to="route(port.path)">
-					{{ port.path }}
-				</router-link>
-				<div class="port-status" v-bind:class="portStatus(port.path)"></div>
+		<fieldset>
+			<legend>Ports</legend>
+			<div class="port-list" v-if="testPortList.length > 0">
+				<div v-for="port in testPortList" :key="port.path" class="port-list-item">
+					<router-link class="link" :to="route(port.path)">
+						{{ port.path }}
+					</router-link>
+					<div class="port-status" v-bind:class="portStatus(port.path)"></div>
+				</div>
 			</div>
-		</div>
+			<div v-else>
+				<p class="warning message">No serial ports detected</p>
+			</div>
+		</fieldset>
 		<router-view></router-view>
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import gql from 'graphql-tag';
 
 export default Vue.extend({
 	data: () => ({
-		ports: [
-			{ path: 'COM5'},
-			{ path: 'COM3'},
-			{ path: 'COM1'},
+		listPorts: new Array<Object>(),
+		listOpenPorts: new Array<string>(),
+		testPortList: [
+			{path: 'COM0'},
+			{path: 'COM1'},
+			{path: 'COM2'},
+			{path: 'COM5'},
 		],
 	}),
-	methods: {
-		portStatus(portPath?: string) {
-			return portPath === 'COM3' ? 'port--open' : 'port--closed';
+	apollo: {
+		listPorts: {
+			query: gql`query listPorts { listPorts { path }}`,
+			pollInterval: 250,
 		},
-		route: (path: string) => {
+		listOpenPorts: {
+			query: gql`query listOpenPorts { listOpenPorts }`,
+			pollInterval: 250,
+		}
+	},
+	methods: {
+		portStatus(portPath: string) {
+			for (let item of this.listOpenPorts) {
+				console.log(item);
+				if (item === portPath) {
+					return 'port--open';
+				}
+			}
+			return 'port--closed';
+			// return [...this.listOpenPorts].find((item) => item === portPath)  ? 'port--open' : 'port--closed';
+		},
+		route(path: string) {
 			return `/dashboard/serial-ports/${path}`;
 		},
 	},
