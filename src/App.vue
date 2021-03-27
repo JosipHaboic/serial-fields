8c4a747ebe8d42574ed386e9cd23f8e513958820
jhaboic <josiphaboic@gmail.com>
2021-03-27 16:23:00 +0100

    refactor top navigation

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
   <div id="app">
-    <top-nav :links="links"></top-nav>
+    <top-nav :links="links" :isOpen="topNavIsOpen">
+		  <nav-icon id="nav-icon" :onToggle="this.toggleMainMenu"/>
+    </top-nav>
+
     <router-view/>
   </div>
 </template>
 
 <script lang="ts">
-import TopNavigation from './components/navigation/TopNavigation.vue';
 import Vue from 'vue';
+import TopNavigation from './components/navigation/TopNavigation.vue';
+import NavIcon from './components/navigation/NavIcon.vue';
 
 export default Vue.extend({
   components: {
     'top-nav': TopNavigation,
+    'nav-icon': NavIcon,
   },
   data: () => ({
+      topNavIsOpen: false,
       links: [
         {href: '/', class: 'link', text: 'Home'},
         {href: '/dashboard', class: 'link', text: 'Dashboard'},
         {href: '/about', class: 'link', text: 'About'},
       ],
     }),
-});
+    methods: {
+		  toggleMainMenu(state: boolean) {
+			  if (this !== undefined) {
+				  this.topNavIsOpen = state;
+			  }
+		  },
+	  },
+  },
+);
 </script>
 
 <style>
--- src/components/navigation/TopNavigation.vue
+++ src/components/navigation/TopNavigation.vue
@@ -1,38 +1,27 @@
 <template>
-	<div id="top-nav-container" v-bind:class="{'top-nav--closed': this.navOpen}">
-		<div id="top-nav" v-bind:class="{'top-nav--closed': this.navOpen}">
-			<router-link v-for="link in links" :key="link.text" :to="link.href" :class="link.class" :exact=true>
-				{{ link.text }}
-			</router-link>
-		</div>
-		<nav-icon id="nav-icon" :onToggle="this.toggle"/>
+	<div id="top-nav" :class="{'top-nav--closed': this.$props.isOpen}">
+		<router-link v-for="link in links" :key="link.text" :to="link.href" :class="link.class" :exact=true>
+			{{ link.text }}
+		</router-link>
+		<slot></slot>
 	</div>
 </template>
 
 
 <script>
 import Vue from 'vue';
-import NavIcon from './NavIcon.vue';
+
 
 export default Vue.extend({
 	name: 'TopNavigation',
-	components: {
-		'nav-icon': NavIcon,
-	},
-	data: () => ({
-		navOpen: false,
-	}),
 	props: {
 		links: {
 			type: Array,
 			required: true,
 		},
-	},
-	methods: {
-		toggle(state) {
-			if (this !== undefined) {
-				this.navOpen = state;
-			}
+		isOpen: {
+			type: Boolean,
+			default: true,
 		},
 	},
 });
