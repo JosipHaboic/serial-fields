7e1a990ef2c92fa329fd438134c0c9e3e34d0162
jhaboic <josiphaboic@gmail.com>
2021-03-28 15:51:05 +0200

    use module technique

---
+++ src/views/dashboard/serial-port/SerialPort.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="container is-centered">
+		<div class="col-6 is-centered">
+			<div class="row is-column">
+				<div class="col-6 is-centered">
+					<h2>Port info</h2>
+				</div>
+
+				<div class="col-6 is-centered">
+					<p>blab fklsfjskflsjfs kkada</p>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Vue from 'vue';
+export default Vue.extend({
+});
+</script>
---
+++ src/views/dashboard/serial-port/SerialPorts.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<port-list :ports="testPortList"/>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import gql from 'graphql-tag';
+import PortList from '@/components/port/PortList.vue';
+
+export default Vue.extend({
+	components: {
+		'port-list': PortList,
+	},
+	data: () => ({
+		listPorts: new Array<object>(),
+		testPortList: [
+			{path: 'COM0'},
+			{path: 'COM1'},
+			{path: 'COM2'},
+			{path: 'COM5'},
+		],
+	}),
+	apollo: {
+		listPorts: {
+			query: gql`query listPorts { listPorts { path }}`,
+			pollInterval: 250,
+		},
+	},
+});
+</script>
