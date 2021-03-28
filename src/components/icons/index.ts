c9e9c97ec85108b8455b45ecd2ba6047085ece71
jhaboic <josiphaboic@gmail.com>
2021-03-28 09:01:31 +0200

    add dashboard icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,10 +2,12 @@ import USBInterfaceIcon from './USBInterfaceIcon.vue';
 import CheckMarkIcon from './CheckMarkIcon.vue';
 import WiFiIcon from './WiFiIcon.vue';
 import SettingsIcon from './SettingsIcon.vue';
+import DashboardIcon from './DashboardIcon.vue';
 
 export const icons = {
 	'usb-interface': USBInterfaceIcon,
 	'check-mark': CheckMarkIcon,
 	'wi-fi': WiFiIcon,
 	'settings': SettingsIcon,
+	'dashboard': DashboardIcon,
 };
