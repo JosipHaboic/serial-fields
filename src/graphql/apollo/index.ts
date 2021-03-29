80a9c4063b6137fb5de39d8602398c32ce0495a7
jhaboic <josiphaboic@gmail.com>
2021-03-29 23:47:51 +0200

    addd error handler for better error report

--- src/graphql/apollo/index.ts
+++ src/graphql/apollo/index.ts
@@ -1,6 +1,23 @@
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+
+// Error Handling
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+	if (graphQLErrors) {
+		graphQLErrors.map(({ message, locations, path }) =>
+			// tslint:disable-next-line
+			console.log(
+				`[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(locations)}, Path: ${path}`,
+			),
+		);
+	}
+	if (networkError) {
+		// tslint:disable-next-line
+		console.log(`[Network error]: ${networkError}`);
+	}
+});
 
 const httpLink = createHttpLink({
 	uri: 'http://localhost:3000/graphql',
@@ -9,6 +26,6 @@ const httpLink = createHttpLink({
 const cache = new InMemoryCache();
 
 export const apolloClient = new ApolloClient({
-	link: httpLink,
+	link: errorLink.concat(httpLink),
 	cache,
 });
